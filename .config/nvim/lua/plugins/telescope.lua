2407bd39bff91e63eb1bbb872c3cdf9b4cb1c8c3
Miles Kent <miles.kent@gtri.gatech.edu>
2026-01-29 10:38:14 -0500

    Edit telescope plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,11 +5,17 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            pickers = {
+                colorscheme = {
+                    enable_preview = true
+                }
+            }
+        })
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep" })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffers" })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help Tags" })
     end
 }
