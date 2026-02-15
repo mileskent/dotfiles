7fe50ed66c9802b5d6bd61ab409fb172ea2c0270
mileskent <mileskent13@gmail.com>
2026-02-15 11:16:23 -0500

    TS-hip

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,20 @@ return {
     branch = 'master',
     lazy = false,
     build = ":TSUpdate",
-    ensure_installed = {
-        "lua", "c", "markdown", "markdown_inline",
-        "bash", "c_sharp", "cmake", "cpp", "cuda",
-        "diff", "rust"
-    }
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "lua", "c", "markdown", "markdown_inline",
+                "bash", "c_sharp", "cmake", "cpp", "cuda",
+                "diff", "rust"
+            },
+            highlight = { enable = true }, -- Don't forget to enable highlighting!
+        })
+        vim.treesitter.language.register("cuda", "hip")
+        vim.filetype.add({
+            extension = {
+                hip = "cuda",
+            },
+        })
+    end
 }
