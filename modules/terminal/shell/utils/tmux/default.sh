bc902f2e0c765bbc9f2f5f78231f3bd9e823a198
mileskent <mileskent13@gmail.com>
2026-03-16 15:47:44 -0400

    tmux unicode

--- modules/terminal/shell/utils/tmux/default.sh
+++ modules/terminal/shell/utils/tmux/default.sh
@@ -6,3 +6,11 @@ TARGET="$HOME/.config/$PKG"
 echo "Setting up: $PKG"
 rm -rf $TARGET && mkdir -p $TARGET
 stow -v -t $TARGET config
+#
+# bash
+for f in "$PWD"/bash/*; do
+    if [ -f "$f" ]; then
+        echo "  -> Sourcing: $(basename "$f")"
+        source "$f"
+    fi
+done
